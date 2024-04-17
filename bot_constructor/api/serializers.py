9cea629695618a15fbcde13be761a0865af3f670
Geroy4ik <klevo2004@gmail.com>
2024-04-18 00:04:21 +0300

    удалены лишние поля сериализаторов

--- bot_constructor/api/serializers.py
+++ bot_constructor/api/serializers.py
@@ -1,4 +1,6 @@
 # type:ignore
+from typing import IO
+
 from apps.bot_management.models import TelegramBot, TelegramBotAction, TelegramBotFile
 from rest_framework import serializers, validators
 
@@ -6,7 +8,6 @@ from rest_framework import serializers, validators
 class TelegramBotCreateSerializer(serializers.ModelSerializer):
     """Сериализатор телеграм бота при создании и обновлении."""
 
-    name = serializers.CharField(max_length=255)
     telegram_token = serializers.RegexField(
         regex=r"^[0-9]{8,10}:[a-zA-Z0-9_-]{35}$",
         validators=(
@@ -17,7 +18,6 @@ class TelegramBotCreateSerializer(serializers.ModelSerializer):
         ),
         write_only=True,
     )
-    api_key = serializers.CharField(max_length=255, required=False)
     api_url = serializers.URLField(required=False)
 
     class Meta:
@@ -28,31 +28,20 @@ class TelegramBotCreateSerializer(serializers.ModelSerializer):
 class TelegramBotShortSerializer(serializers.ModelSerializer):
     """Сериализатор для отображения телеграм бота в списке."""
 
-    name = serializers.CharField(max_length=255)
-    telegram_token = serializers.CharField(max_length=255, write_only=True)
-    is_started = serializers.BooleanField(read_only=True)
-    bot_state = serializers.ChoiceField(TelegramBot.BotState.choices, read_only=True)
-
     class Meta:
         model = TelegramBot
-        fields = ("id", "name", "telegram_token", "is_started", "bot_state")
+        fields = ("id", "name", "is_started", "bot_state")
 
 
 class TelegramBotSerializer(TelegramBotShortSerializer):
     """Сериализатор для детального отображения телеграм бота."""
 
-    description = serializers.CharField(max_length=1000, required=False)
-    api_key = serializers.CharField(max_length=255)
     api_url = serializers.URLField()
-    api_availability = serializers.BooleanField(read_only=True)
-    created_at = serializers.DateTimeField(read_only=True)
-    started_at = serializers.DateTimeField(read_only=True)
 
     class Meta:
         model = TelegramBot
         fields = (
             "name",
-            "telegram_token",
             "description",
             "bot_state",
             "api_key",
@@ -97,25 +86,27 @@ class TelegramBotActionSerializer(serializers.ModelSerializer):
     telegram_bot = serializers.PrimaryKeyRelatedField(
         queryset=TelegramBot.objects.all()
     )
-    name = serializers.CharField(max_length=255)
-    description = serializers.CharField(max_length=1000, required=False)
     command_keyword = serializers.RegexField(r"^/[a-zA-Z0-9_]{1,32}$")
-    message = serializers.CharField(max_length=1000, required=False)
-    api_key = serializers.CharField(max_length=255, required=False)
     api_url = serializers.URLField(required=False)
     position = serializers.IntegerField(min_value=1, max_value=30)
-    is_active = serializers.BooleanField()
     files = TelegramFileSerializer(many=True, required=False)
 
     def create(self, validated_data):
+        """
+        Дополнительно модифицированый стандартный метод create().
+        Добавлена возможность обработки сразу нескольких файлов одним пакетом.
+
+        """
 
         action_instance = TelegramBotAction.objects.create(**validated_data)
         if "files" in self.context:
-            files = self.context.get("files")
+            files: list[IO] = self.context.get("files")
+            telegram_files: list = []
             for file in files:
-                TelegramBotFile.objects.create(
-                    file=file, telegram_action=action_instance
+                telegram_files.append(
+                    TelegramBotFile(file=file, telegram_action=action_instance)
                 )
+            TelegramBotFile.objects.bulk_create(telegram_files)
         return action_instance
 
     class Meta:
