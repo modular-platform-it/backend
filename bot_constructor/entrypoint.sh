4fa88ebc4bb7b886ae85c56caee65ed1ac1374a0
sihuannewrise <sihuannewrise@gmail.com>
2024-04-24 21:36:14 +0300

    entrypoint moved to bot_constructor folder

--- bot_constructor/entrypoint.sh
+++ bot_constructor/entrypoint.sh
@@ -2,4 +2,4 @@ python manage.py migrate
 
 python manage.py collectstatic --clear --noinput
 
-gunicorn --bind 0:8000 bot_constructor.wsgi.application
+gunicorn --bind 0.0.0.0:8000 bot_constructor.wsgi.application
