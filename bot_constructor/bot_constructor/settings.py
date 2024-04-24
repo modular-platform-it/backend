d6c4dfb3a0268e8759538e3066d21f8a666f3725
sihuannewrise <sihuannewrise@gmail.com>
2024-04-24 21:30:19 +0300

    default storage added to settings.py

--- bot_constructor/bot_constructor/settings.py
+++ bot_constructor/bot_constructor/settings.py
@@ -147,6 +147,9 @@ STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
