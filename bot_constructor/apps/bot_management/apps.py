b9301d13889e5643325cdfe7dccdea25ef915324
sihuannewrise <sihuannewrise@gmail.com>
2024-04-26 20:18:45 +0300

    no apps module error correction

--- bot_constructor/apps/bot_management/apps.py
+++ bot_constructor/apps/bot_management/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class BotManagementConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "bot_management"
+    name = "apps.bot_management"
