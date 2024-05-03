f3324fbb630aa6ff5aa03e69282277f5ad632519
sihuannewrise <sihuannewrise@gmail.com>
2024-05-03 19:42:02 +0300

    use sqlite for GH tests

--- bot_constructor/bot_constructor/settings.py
+++ bot_constructor/bot_constructor/settings.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from pathlib import Path
 
 from django.core.management.utils import get_random_secret_key
@@ -81,11 +82,11 @@ TEMPLATES = [
 WSGI_APPLICATION = "bot_constructor.wsgi.application"
 
 
-if os.getenv("USE_SQLITE", "False") == "True":
+if sys.argv[1] == "test":
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
-            "NAME": str(BASE_DIR / "db.sqlite3"),
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
         }
     }
 else:
