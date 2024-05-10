9b05967bc5fe6e8950a405902ace4779b87a247d
sihuannewrise <sihuannewrise@gmail.com>
2024-05-10 20:42:48 +0300

    bot-server wflow

--- bot_server/api.py
+++ bot_server/api.py
@@ -1,11 +1,11 @@
-# type:ignore
 import asyncio
+
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 from bots import TelegramBot
 from db import Connection
-from fastapi import FastAPI
 from models import Bots
-from pydantic import BaseModel
 
 """Шина общения и управление ботом"""
 app = FastAPI()
