3c022c5fe161b97dab1963b80aca1d0ef80f19be
Paymir <nikox12@mail.ur>
2024-05-10 17:17:10 +0300

    Fix

--- bot_server/db.py
+++ bot_server/db.py
@@ -22,15 +22,15 @@ class DB:
     """Создание в БД таблиц"""
 
     def __init__(self):
-        self.conn = Connection()
+        self.connection = Connection()
 
     def to_db(
         self,
     ):
         Base.metadata.create_all(
-            bind=self.conn.engine,
+            bind=self.connection.engine,
         )
-        self.conn.session.commit()
+        self.connection.session.commit()
 
 
 def run():
