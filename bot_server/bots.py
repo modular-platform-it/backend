6939d8f9f52ef428db37831ca0e76b295e744afe
Paymir <nikox12@mail.ur>
2024-05-10 16:05:48 +0300

    Запуск нескольких ботов из БД

--- bot_server/bots.py
+++ bot_server/bots.py
@@ -2,30 +2,33 @@ import asyncio
 
 from aiogram import Bot, Dispatcher
 from aiogram.types import BotCommand
+
 from db import Connection
-from handlers import router
 from models import Bots
+from handlers import Handlers
 
 
-class TelegramBot:
-    def __init__(self, bot_id):
-        self.bot_id = bot_id
-        self.connection = Connection()
-        self.bot_data = self.connection.session.query(Bots).filter(Bots.id == bot_id).first()
+class TelegramBot(Handlers):
+    """Основана телеграмм Бота"""
+    def __init__(self, bot_data: Bots):
+        super().__init__()
+        self.bot_data = bot_data
         self.token = self.bot_data.token
         self.bot = Bot(token=self.token)
         self.dispatcher = Dispatcher()
-        # self.dispatcher.include_router(router1)
         self.commands = [
             BotCommand(command="/start", description=f"Start the bot {self.bot_data.name}"),
         ]
+        self.dispatcher.include_router(self.router)
 
     async def start(self):
-        print('Bot started')
+        print('Bot starting')
         await self.bot.set_my_commands(commands=self.commands)
         await self.dispatcher.start_polling(self.bot)
 
 
 if __name__ == "__main__":
-    bot = TelegramBot(bot_id=6)
+    connection = Connection()
+    bot_data = connection.session.query(Bots).filter(Bots.id==6).first()
+    bot = TelegramBot(bot_data=bot_data)
     asyncio.run(bot.start())
