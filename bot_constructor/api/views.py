f9b8329eb271fbd680e2b40ba26502ed758e2500
Olesya Orlenko <olees.orlenko@yandex.ru>
2024-05-21 00:26:07 +0300

    fix: add permission_classes

--- bot_constructor/api/views.py
+++ bot_constructor/api/views.py
@@ -1,5 +1,5 @@
 from datetime import datetime as dt
-from typing import Any
+from typing import Any, Type
 
 import requests
 from api.drf_spectacular.drf_serializers import (
@@ -35,6 +35,7 @@ from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.filters import OrderingFilter
 from rest_framework.parsers import FormParser, MultiPartParser
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
 
@@ -159,11 +160,13 @@ class TelegramBotViewSet(viewsets.ModelViewSet):
     )
     ordering = ("-created_at",)
     lookup_field = "pk"
+    permission_classes = (IsAuthenticated,)
 
     def get_serializer_class(
         self,
-    ) -> type(TelegramBotShortSerializer) | type(
-        TelegramBotSerializer | type(TelegramBotCreateSerializer)
+    ) -> (
+        Type[TelegramBotShortSerializer]
+        | Type[TelegramBotSerializer | TelegramBotCreateSerializer]
     ):
         if self.action == "list":
             return TelegramBotShortSerializer
@@ -287,6 +290,7 @@ class TelegramBotActionViewSet(viewsets.ModelViewSet):
     serializer_class = TelegramBotActionSerializer
     parser_classes = (FormParser, MultiPartParser)
     lookup_field = "pk"
+    permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
         return TelegramBotAction.objects.filter(
@@ -430,6 +434,7 @@ class TelegramBotActionFileViewSet(viewsets.ModelViewSet):
 
     queryset = TelegramBotFile.objects.all()
     serializer_class = TelegramFileSerializer
+    permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
         return TelegramBotFile.objects.filter(
--- bot_constructor/bot_constructor/settings.py
+++ bot_constructor/bot_constructor/settings.py
@@ -45,6 +45,7 @@ MIDDLEWARE = [
     "allauth.account.middleware.AccountMiddleware",
 ]
 REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAuthenticated"],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
