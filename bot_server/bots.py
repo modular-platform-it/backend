0facf3b79b303b8ca0a94957e3cbab64435c718d
Olesya Orlenko <olees.orlenko@yandex.ru>
2024-05-20 23:46:14 +0300

    feat:add handler

--- bot_server/bots.py
+++ bot_server/bots.py
@@ -1,8 +1,7 @@
-
+import handlers
 from aiogram import Bot, Dispatcher
 from aiogram.types import BotCommand
 
-import handlers
 
 class BaseTelegramBot:
     """Основа телеграмм Бота"""
@@ -12,14 +11,24 @@ class BaseTelegramBot:
         self.bot_data = bot_data
         self.token = self.bot_data.telegram_token
         self.bot = Bot(token=self.token)
+        # self.api_url = f"https://{self.bot_data.api_url}"
+        self.api_url = self.bot_data.api_url
+        # self.actions = connection.session.query(TelegramBot).filter(TelegramBot.id == self.bot.id).all()
         self.dispatcher = Dispatcher()
         self.commands = [
             BotCommand(
                 command="/start", description=f"Start the bot {self.bot_data.name}"
             ),
         ]
-
-        actions = []
+        actions = [
+            {
+                "name": "Handlers",
+                "parameters": {
+                    "commands": "/get_list",
+                },
+            },
+        ]
+        # actions = []
 
         for action in actions:
             router = getattr(handlers, action["name"])().router
@@ -33,9 +42,11 @@ class BaseTelegramBot:
 
 if __name__ == "__main__":
     import asyncio
+
     from db import Connection
     from models import TelegramBot
+
     connection = Connection()
-    bot_data = connection.session.query(TelegramBot).filter(TelegramBot.id == 3).first()
+    bot_data = connection.session.query(TelegramBot).filter(TelegramBot.id == 2).first()
     bot = BaseTelegramBot(bot_data=bot_data)
     asyncio.run(bot.start())
--- bot_server/handlers.py
+++ bot_server/handlers.py
@@ -1,8 +1,24 @@
-from aiogram import F, Router
+import json
+
+import requests
+from aiogram import Bot, Dispatcher, F, Router, types
 from aiogram.filters import Command
 from aiogram.types import Message
+from fastapi import HTTPException
+from models import ItemList, TelegramBot
 
-# from api import app
+bot = TelegramBot()
+
+
+async def get_list() -> ItemList:
+    """Получить список из другого приложения по API."""
+    # response = requests.get("https://api.xwick.ru/v1/bots/")
+    response = requests.get(bot.api_url)
+    if response.status_code != 200:
+        raise HTTPException(status_code=response.status_code, detail=response.text)
+    items = [value["name"] for value in response.json()]
+    return ItemList(items=items)
+
 
 class Handlers:
     """Логика работы ботов"""
@@ -12,13 +28,16 @@ class Handlers:
 
         @self.router.message(Command("start"))
         async def start_handler(msg: Message):
-            await msg.answer(
-                "Привет! Я помогу тебе узнать твой ID, просто отправь мне любое сообщение"
-            )
+            await msg.answer("Привет!")
 
-        @self.router.message()
-        async def message_handler(msg: Message):
-            await msg.answer(f"Твой ID: {msg.from_user.id}")
+        @self.router.message(Command("get_list"))
+        async def get_list_handler(msg: Message):
+            item_list = await get_list()
+            await msg.answer(f"Список из вашего приложения: {item_list.items}")
+
+        @self.router.message(Command("stop"))
+        async def stop_handler(msg: Message):
+            await msg.answer("До новых встреч!")
 
 
 class SendMassage:
