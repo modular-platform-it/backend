c06e0c30410bd48350ef451d914a82ce546afdbe
sihuannewrise <sihuannewrise@gmail.com>
2024-05-26 11:01:18 +0300

    pg db init

---
+++ infra/services/init-multi-db.sh
@@ -0,0 +1,34 @@
+: '
+Script to init multiple postgres databases. Path to place the file:
+scp infra/services/init-multi-db.sql sihuan@xwick.ru:/home/sihuan/modular/dockume/postgres
+'
+#!/bin/bash
+
+set -e
+set -u
+
+function create_database() {
+	database=$1
+    password=$2
+    echo "Creating user and database '$database' with password '$password'"
+    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+      CREATE USER $database with encrypted password '$password';
+      CREATE DATABASE $database;
+      GRANT ALL PRIVILEGES ON DATABASE $database TO $database;
+EOSQL
+}
+
+if [ -n "$POSTGRES_MULTI_DB" ]; then
+  echo "Multiple database creation requested: $POSTGRES_MULTI_DB"
+  for db in $(echo $POSTGRES_MULTI_DB | tr ',' ' '); do
+    user=$(echo $db | awk -F":" '{print $1}')
+    pswd=$(echo $db | awk -F":" '{print $2}')
+    if [[ -z "$pswd" ]]
+    then
+      pswd=$user
+    fi
+    echo "user is $user and pass is $pswd"
+    create_database $user $pswd
+  done
+  echo "Multiple databases created!"
+fi
