3875a2d04b13193ab784badf8be8027cc855a002
sihuannewrise <sihuannewrise@gmail.com>
2024-06-04 21:13:11 +0300

    setuptools added

--- testing_app/testing_app/settings.py
+++ testing_app/testing_app/settings.py
@@ -9,7 +9,6 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 SECRET_KEY = os.getenv("SECRET_KEY_DJANGO", get_random_secret_key())
 
 DEBUG = os.getenv("DEBUG", "False") == "True"
