c62bc0d5e4e045fee6cfe6288d47e2546c98e1a4
Nikki Nikkonor <110825664+Paymir121@users.noreply.github.com>
2024-06-11 20:48:57 +0400

    fix

--- bot_server/handlers.py
+++ bot_server/handlers.py
@@ -132,63 +132,61 @@ class GetItem:
             await msg.answer(f"Нужный вам обьект: {gen}")
 
 
-class RandomWordLearnListHandler:
-    def __init__(self, bot_data, action, connection):
-        """Словарик для запоминания слов"""
-        self.bot_data = bot_data
-        self.router = Router()
-        self.action = action
-        self.connection = connection
-        self.commands = [
-            BotCommand(command="/add_word", description=f"Добавить новое слово"),
-            BotCommand(
-                command="/get_words_list",
-                description=f"Получить список ваших слов на сегодня",
-            ),
-        ]
-        self.words = self.action.data or []
-        self.requirement_count_word = 5
-        with self.connection as session:
-            self.action.data = {
-                "words": "self.words",
-            }
-            session.commit()
-
-        class WordState(StatesGroup):
-            word = State()
-            translate = State()
-
-        @self.router.message(Command("add_word"))
-        async def get_start_add_handler(msg: Message, state: FSMContext):
-            """Добавление нового слова и его перевод в словарик"""
-            await state.set_state(WordState.word)
-            await msg.answer("Введите слово")
-
-        @self.router.message(WordState.word)
-        async def get_word_handler(msg: Message, state: FSMContext):
-            await state.update_data(word=msg.text)
-            await state.set_state(WordState.translate)
-            await msg.answer("Введите его перевод")
-
-        @self.router.message(WordState.translate)
-        async def get_translate_handler(msg: Message, state: FSMContext):
-            await state.update_data(translate=msg.text)
-            await state.set_state(WordState.translate)
-            data = await state.get_data()
-            self.words.append(data)
-            self.action.data = self.words
-            await state.clear()
-            await msg.answer("Слово добавлено")
-
-        @self.router.message(Command("get_words_list"))
-        async def get_word_list_handler(msg: Message):
-            """Получение списка из 5 случайных слов"""
-            text = ""
-            if self.requirement_count_word < len(self.words):
-                for item in range(len(self.words)):
-                    word = random.choice(self.words)
-                    text += word["word"] + " : " + word["translate"] + "\n"
-            else:
-                for item in self.words:
-                    text += item["word"] + " : " + item["translate"] + "\n"
-            await msg.answer(f"Список ваших слов\n{text}")
+# class RandomWordLearnListHandler:
+#     def __init__(self, bot_data, action, connection):
+#         """Словарик для запоминания слов"""
+#         self.bot_data = bot_data
+#         self.router = Router()
+#         self.action = action
+#         self.connection = connection
+#         self.commands = [
+#             BotCommand(
+#                 command="/add_word",
+#                 description=f"Добавить новое слово"
+#             ),
+#             BotCommand(
+#                 command="/get_words_list",
+#                 description=f"Получить список ваших слов на сегодня",
+#             ),
+#         ]
+#         self.words = self.action.data or []
+#         self.requirement_count_word = 5
+#
+#         class WordState(StatesGroup):
+#             word = State()
+#             translate = State()
+#
+#         @self.router.message(Command("add_word"))
+#         async def get_start_add_handler(msg: Message, state: FSMContext):
+#             """Добавление нового слова и его перевод в словарик"""
+#             await state.set_state(WordState.word)
+#             await msg.answer("Введите слово")
+#
+#         @self.router.message(WordState.word)
+#         async def get_word_handler(msg: Message, state: FSMContext):
+#             await state.update_data(word=msg.text)
+#             await state.set_state(WordState.translate)
+#             await msg.answer("Введите его перевод")
+#
+#         @self.router.message(WordState.translate)
+#         async def get_translate_handler(msg: Message, state: FSMContext):
+#             await state.update_data(translate=msg.text)
+#             await state.set_state(WordState.translate)
+#             data = await state.get_data()
+#             self.words.append(data)
+#             self.action.data = self.words
+#             await state.clear()
+#             await msg.answer("Слово добавлено")
+#
+#         @self.router.message(Command("get_words_list"))
+#         async def get_word_list_handler(msg: Message):
+#             """Получение списка из 5 случайных слов"""
+#             text = ""
+#             if self.requirement_count_word < len(self.words):
+#                 for item in range(len(self.words)):
+#                     word = random.choice(self.words)
+#                     text += word["word"] + " : " + word["translate"] + "\n"
+#             else:
+#                 for item in self.words:
+#                     text += item["word"] + " : " + item["translate"] + "\n"
+#             await msg.answer(f"Список ваших слов\n{text}")
